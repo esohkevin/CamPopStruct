35155390c711d7268e4ef0f79fb9744014269721
esohkevin <esohkevin4@gmail.com>
2023-09-11 11:37:18 +0200

    edit

--- flk/prep.R
+++ flk/prep.R
@@ -4,20 +4,20 @@ require(data.table)
 
 args <- commandArgs(TRUE)
 
-fn <- args[1]
-pr <- args[2]
-#pe <- paste0(pr, ".ped")
-#ma <- paste0(pr, ".map")
-fa <- paste0(pr, ".fam")
-#outpe <- paste0(pr, "flk.ped")
-#outma <- paste0(pr, "flk.map")
-outfa <- paste0(pr, "flk.fam")
+ethnicity <- args[1]
+bedprefix <- args[2]
+#pe <- paste0(bedprefix, ".ped")
+#ma <- paste0(bedprefix, ".map")
+famname <- paste0(bedprefix, ".fam")
+#outpe <- paste0(bedprefix, "flk.ped")
+#outma <- paste0(bedprefix, "flk.map")
+outfa <- paste0(bedprefix, "flk.fam")
 
-eth <- as.data.frame(fread(fn, header=F, nThread = 15))
-fam <- as.data.frame(fread(fa, header = F, nThread = 20))
+eth <- as.data.frame(fread(ethnicity, header=F, nThread = 15))
+fam <- as.data.frame(fread(famname, header = F, nThread = 20))
 fam$V5 <- as.integer("1")
 eth <- data.frame(V3=eth$V3,V2=eth$V2)
-hap <- merge(eth, fam, by = "V2")
+hap <- merge(eth, fam, by = "V2", sort=F)
 hap <- hap[,-c(1)]
 fwrite(hap, file = outfa, col.names=F, sep = " ", 
 	row.names=F, quote=F, nThread = 30, buffMB = 10)
